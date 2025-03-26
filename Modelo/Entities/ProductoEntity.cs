4c0bc356bf6e188da3a530f491157668be025827
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-03-26 08:57:21 -0500

    Se realizo productoEntity

--- Modelo/Entities/ProductoEntity.cs
+++ Modelo/Entities/ProductoEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,32 @@ namespace Modelo.Entities
 {
     class ProductoEntity
     {
+        public int codigo_producto { get; set; }
+
+        public string nombre_producto { get; set; }
+
+        public string descripcion_producto { get; set; }
+
+        public string marca_producto { get; set;  }
+
+        public string modelo_producto { get; set; }
+
+        public decimal precio_producto { get; set; }
+
+        public string material_prodcuto { get; set; }
+
+        public string tamañoTalla_producto { get; set; }
+
+        public string color_producto { get; set;  }
+
+        public int stock_producto { get; set; }
+
+        //Llave foranea
+
+        public int fkCodigo_categoria { get; set; }
+        public CategoriaEntity categoria { get; set; }
+
+   
+
     }
 }

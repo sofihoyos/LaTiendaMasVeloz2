021e7ebf1518802bb6e4fb6dde4777c126947352
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-03-26 08:47:50 -0500

    Se realizo proveedorEntity

--- Modelo/Entities/ProveedorEntity.cs
+++ Modelo/Entities/ProveedorEntity.cs
@@ -8,5 +8,14 @@ namespace Modelo.Entities
 {
     class ProveedorEntity
     {
+        public int nit_proveedor { get; set; }
+
+        public string nombre_proveedor {get; set;}
+
+        public string correo_proveedor { get; set; }
+
+        public string telefono_proveedor { get; set; }
+
+      
     }
 }

f0cf87f293baa974abb63cc7e985c52c500794cd
sofihoyos <169062995+sofihoyos@users.noreply.github.com>
2025-04-01 19:13:28 -0500

    Se creo el metodo eliminar

--- Modelo/BaseDatos.cs
+++ Modelo/BaseDatos.cs
@@ -25,6 +25,15 @@ namespace Modelo
             return filasAfectadas;
         }
 
+        public int EliminarCategoria(int codigo_categoria)
+        {
+            MySqlCommand cmd = GetConnection().CreateCommand();
+            cmd.CommandText = "DELETE FROM categoria WHERE codigo_categoria = '" + codigo_categoria + "'";
+            int filasAfectadas = cmd.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+
         //CREDENCIALES
         public int GuardarCredenciales(int id_credenciales, string usuario, string contraseña,int fkId_persona)
         {
@@ -47,6 +56,15 @@ namespace Modelo
             return filasAfectadas;
         }
 
+        public int EliminarFactura(int id_factura)
+        {
+            MySqlCommand cmd = GetConnection().CreateCommand();
+            cmd.CommandText = "DELETE FROM factura WHERE id_factura = '" + id_factura + "'";
+            int filasAfectadas = cmd.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+
 
         //PERSONA
         public int GuardarPersona(int id_persona, string nombre, string telefono, string correo, TipoPersona tipo_persona)
@@ -59,12 +77,30 @@ namespace Modelo
             return filasAfectadas;
         }
 
+        public int EliminarPersona(int id_persona)
+        {
+            MySqlCommand cmd = GetConnection().CreateCommand();
+            cmd.CommandText = "DELETE FROM persona WHERE id_persona = '" + id_persona + "'";
+            int filasAfectadas = cmd.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+
         //PRODUCTO
         public int GuardarProducto(int codigo_producto, string nombre_producto, string descripcion_producto, int stock_producto, int fkCodigo_categoria)
         {
             MySqlCommand cmd = GetConnection().CreateCommand();
             cmd.CommandText = "INSERT INTO producto (codigo_producto, nombre_producto, descripcion_producto, stock_producto, fkCodi_categoria) " +
                 "VALUES ('" + codigo_producto + "','" + nombre_producto + "','" + descripcion_producto + "','" + stock_producto + "','" + fkCodigo_categoria + "')";
+            int filasAfectadas = cmd.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+
+        public int EliminarProducto(int codigo_producto)
+        {
+            MySqlCommand cmd = GetConnection().CreateCommand();
+            cmd.CommandText = "DELETE FROM producto WHERE codigo_producto = '" + codigo_producto + "'";
             int filasAfectadas = cmd.ExecuteNonQuery();
 
             return filasAfectadas;

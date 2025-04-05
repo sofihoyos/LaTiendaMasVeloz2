6b35468476ef2a837342837d94dce55c7a762968
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-04-05 11:17:11 -0500

    Creación del crud cliente y proveedor

---
+++ Principal/CrearCliente.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Principal
+{
+    partial class CrearCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Columna1 = new DataGridViewTextBoxColumn();
+            ID = new DataGridViewTextBoxColumn();
+            Cédula = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Teléfono = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(54, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Tabla Cliente";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(76, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Cédula / NIT:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(172, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(397, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 17);
+            label3.TabIndex = 3;
+            label3.Text = "Nombre:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(503, 139);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(163, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(397, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 17);
+            label4.TabIndex = 5;
+            label4.Text = "Teléfono:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(503, 194);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(163, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(76, 200);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 17);
+            label5.TabIndex = 7;
+            label5.Text = "Correo:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(172, 200);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Columna1, ID, Cédula, Nombre, Teléfono });
+            dataGridView1.Location = new Point(104, 289);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(543, 150);
+            dataGridView1.TabIndex = 9;
+            // 
+            // Columna1
+            // 
+            Columna1.HeaderText = "Column1";
+            Columna1.Name = "Columna1";
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            // 
+            // Cédula
+            // 
+            Cédula.HeaderText = "Cédula/nit";
+            Cédula.Name = "Cédula";
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            // 
+            // Teléfono
+            // 
+            Teléfono.HeaderText = "Teléfono";
+            Teléfono.Name = "Teléfono";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(79, 493);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(196, 493);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "Actualizar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(317, 493);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 12;
+            button3.Text = "Eliminar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(625, 493);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 13;
+            button4.Text = "Regresar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // CrearCliente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(739, 540);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CrearCliente";
+            Text = "CrearCliente";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Columna1;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Cédula;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Teléfono;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Principal/CrearProveedor.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Principal
+{
+    partial class CrearProveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Columna1 = new DataGridViewTextBoxColumn();
+            ID = new DataGridViewTextBoxColumn();
+            Cédula = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Teléfono = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(68, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Tabla Proveedor";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(165, 194);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 23);
+            textBox4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(69, 194);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 17);
+            label5.TabIndex = 15;
+            label5.Text = "Correo:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(496, 188);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(163, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(390, 188);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 17);
+            label4.TabIndex = 13;
+            label4.Text = "Teléfono:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(496, 133);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(163, 23);
+            textBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(390, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 17);
+            label3.TabIndex = 11;
+            label3.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(165, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(69, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 17);
+            label2.TabIndex = 9;
+            label2.Text = "Cédula / NIT:";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(615, 481);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 21;
+            button4.Text = "Regresar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(307, 481);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 20;
+            button3.Text = "Eliminar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(186, 481);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 19;
+            button2.Text = "Actualizar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(69, 481);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 18;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Columna1, ID, Cédula, Nombre, Teléfono });
+            dataGridView1.Location = new Point(94, 277);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(543, 150);
+            dataGridView1.TabIndex = 17;
+            // 
+            // Columna1
+            // 
+            Columna1.HeaderText = "Column1";
+            Columna1.Name = "Columna1";
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            // 
+            // Cédula
+            // 
+            Cédula.HeaderText = "Cédula/nit";
+            Cédula.Name = "Cédula";
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            // 
+            // Teléfono
+            // 
+            Teléfono.HeaderText = "Teléfono";
+            Teléfono.Name = "Teléfono";
+            // 
+            // CrearProveedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(739, 537);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CrearProveedor";
+            Text = "CrearProveedor";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Columna1;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Cédula;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Teléfono;
+    }
+}

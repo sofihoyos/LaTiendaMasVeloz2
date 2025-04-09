d6cbdfab52544d5b6aa70a341f2e22327aa0b34d
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-04-09 07:22:13 -0500

    Creación de PersonaController

--- Logica/PersonaController.cs
+++ Logica/PersonaController.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelo;
+using Modelo.Entities;
 
 namespace Logica
 {
     class PersonaController
     {
+        public string GuardarPersona(int id_persona, string nombre, string telefono, string correo, TipoPersona tipo_persona)
+        {
+            string resultado;
+            BaseDatos db = new BaseDatos();
+            int filasInsertadas = db.GuardarPersona( id_persona, nombre, telefono, correo, tipo_persona);
+
+            if (filasInsertadas > 0)
+            {
+                resultado = "Guardado";
+            }
+            else
+            {
+                resultado = "No guardado";
+            }
+            return resultado;
+        }
+
+        public PersonaEntity MostrarPersona()
+        {
+            BaseDatos db = new BaseDatos();
+            PersonaEntity personaActual = db.MostrarPersona();
+            return personaActual;
+        }
     }
 }
